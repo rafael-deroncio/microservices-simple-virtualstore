02e59bbb3c0ec2f9a287697ca447f1dc9726cb06
rderoncio <rafael.deroncio@animaeducacao.com.br>
2023-08-10 22:29:19 -0300

    add settings e secrets

--- virtualstore-service-catalog/src/VirtualStore.Catalog.API/Program.cs
+++ virtualstore-service-catalog/src/VirtualStore.Catalog.API/Program.cs
@@ -1,13 +1,45 @@
+using VirtualStore.Catalog.API.Extensions;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddSecrets();
+
+builder.Services.AddLowerCaseRouting();
+
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddServicesDependencyInjection();
+
+builder.Services.AddRepositoriesDependencyInjection();
+
+builder.Services.AddAPIVersioningCustomSettings(builder.Configuration);
+
+builder.Services.AddSwaggerCustomSettings(builder.Configuration);
+
+builder.Services.AddCorsCustomSettings(builder.Configuration);
+
+builder.Services.AddAuthentication(builder.Configuration);
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddAuthenticationSwaggerJwtBearer();
 
 WebApplication app = builder.Build();
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
 app.UseAuthorization();
+
+app.UseHandleException();
+
+app.UseSwaggerCustomSettings();
+
+app.UseHttpsRedirection();
+
 app.MapControllers();
 
 app.Run();
---
+++ virtualstore-service-catalog/src/VirtualStore.Catalog.API/Settings/CorsSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace VirtualStore.Catalog.API.Settings;
+
+public class CorsSettings
+{
+    public CorsCustomPolicy CorsPolicy { get; set; }
+}
+
+public class CorsCustomPolicy
+{
+    public string Name { get; set; }
+    public string[] AllowedOrigins { get; set; }
+    public string[] AllowedMethods { get; set; }
+    public string[] AllowedHeaders { get; set; }
+}
